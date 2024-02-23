e678158e9c83422de450df5290d08ad28dcf3200
David G <david.giasson@claurendeau.qc.ca>
2024-02-23 14:00:54 -0500

    Ajout du constructeur.

--- Vecteur.java
+++ Vecteur.java
@@ -1,4 +1,12 @@
 public class Vecteur {
     private int[] tableau;
     private int nbElements;
+
+    public static final int RATIO_AGRANDISSEMENT = 2;
+    public static final int TAILLE_INITIALE = 5;
+
+    public Vecteur() {
+        tableau = new int[TAILLE_INITIALE];
+        nbElements = 0;
+    }
 }
