e509644a1cccb68ef83362431aabfc63a40cbfaa
David G <david.giasson@claurendeau.qc.ca>
2024-02-28 14:57:15 -0500

    Ajout des méthodes toString() et ajouter() à la classe Vecteur.

--- src/Vecteur.java
+++ src/Vecteur.java
@@ -9,4 +9,21 @@ public class Vecteur {
         tableau = new int[TAILLE_INITIALE];
         nbElements = 0;
     }
+
+    public String toString() {
+        String str = "[";
+        for (int i = 0; i < nbElements; i++)
+            str += tableau[i] + ", ";
+        return str + "]";
+    }
+
+    public void ajouter(int element) {
+        if (nbElements >= tableau.length)
+            resize();
+        tableau[nbElements++] = element;
+    }
+
+    private void resize() {
+        //TODO Agrandir le tableau de données.
+    }
 }
